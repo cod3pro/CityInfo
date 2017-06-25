4d89b7ab5b7253b13d8c80b2cd9fe7d693c7c202
Bori <borhanalizadeh@gmail.com>
2017-06-25 00:40:12 +0000

    Added Mongoose to it so data can be saved into DB. Added show page to show the detail of each locaiton

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
-var express = require("express");
-var app = express();
-var bodyparser = require("body-parser");
+var express =    require("express"),
+    app =        express(),
+    bodyparser = require("body-parser"),
+    mongoose =   require("mongoose");
+    
+
+mongoose.connect("mongodb://localhost/cityinfo");
+
+//City info schema
+var cityinfoSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+    
+//database model
+var cityInfo = mongoose.model("cityInfo", cityinfoSchema);
+
+// cityInfo.create({
+//     name: "Griffith Observatory", 
+//     image: "http://griffithobservatory.org/slideshow/slide_show00.jpg",
+//     description: "Griffith Observatory is a facility in Los Angeles, California, sitting on the south-facing slope of Mount Hollywood in Los Angeles' Griffith Park. It commands a view of the Los Angeles Basin, including Downtown Los Angeles to the southeast, Hollywood to the south, and the Pacific Ocean to the southwest. The observatory is a popular tourist attraction with an excellent view of the Hollywood Sign, and an extensive array of space and science-related displays. Since the observatory's opening in 1935, admission has been free, in accordance with the benefactor's will, after whom the observatory is named – Griffith J. Griffith."
+// }, function(err, city){
+//     if(err){
+//         console.log("Error happend");
+//     } else {
+//         console.log("City added successfully");
+//         console.log(city);
+//     }
+// });
 
 app.use(bodyparser.urlencoded({extended: true}));
 
@@ -19,30 +46,58 @@ var info = [
 ];
 
 
-
 app.get("/", function(req, res){
    res.render("landing"); 
 });
 
-app.get("/cityinfo", function(req, res){
 
-   res.render("cityinfo", {info: info}); 
+//INDEX - show all city info
+app.get("/cityinfo", function(req, res){
+    cityInfo.find({}, function(err, info){
+       if(err){
+           console.log(err);
+       } else {
+            res.render("cityinfo", {info: info}); 
+       }
+    });
 });
 
 
+//CREATE - add a new info to our db
 app.post("/cityinfo", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
-    var newPlace = {name: name, image: image};
-    info.push(newPlace);
-   res.redirect("/cityinfo"); 
+    var description = req.body.description;
+    var newPlace = {name: name, image: image, description: description};
+    //Add the new city info to the database
+    cityInfo.create(newPlace, function(err, info){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/cityinfo");
+        }
+    });
 });
 
 
+//NEW - show form to add a new city info
 app.get("/cityinfo/new", function(req, res){
    res.render("new.ejs"); 
 });
 
+
+
+//SHOW - show info about one city
+app.get("/cityinfo/:id", function(req, res){
+    cityInfo.findById(req.params.id, function(err, cInfo){
+        if(err){
+            console.log(err);
+        } else {
+            var info = cityInfo.find({id: "id"});
+            res.render("show", {info: cInfo})     
+        }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The city info server has started"); 
